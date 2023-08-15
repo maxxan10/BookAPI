30de7b474fefca8947a76b2a5b3de20ae0524e88
maxxan10 <maxxan14@gmail.com>
2023-08-15 18:59:14 +0200

    Added Jwt token and User authentication

---
+++ BookAPI/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BookAPI.Controllers
+{
+    [EnableCors("AllowOrigin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+
+            _configuration = configuration; 
+
+        }
+
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if(user.Username == "admin" && user.Password == "admin")
+            {
+                _user = new Users { Username = "Admin Adminsson" };
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users users)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], null,
+                expires: DateTime.Now.AddMinutes(2),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticateUser(user);
+            if (user_ != null)
+            {
+                var token  = GenerateToken(user_);
+                response = Ok(new { token = token });
+
+            }
+            return response;
+        }
+    }
+}
--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,10 +1,26 @@
 using BookAPI.Models;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,6 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseCors(options => 
 options.WithOrigins("http://localhost:4200")
 .AllowAnyMethod()
